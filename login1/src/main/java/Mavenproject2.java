ae09b5553df26e32f1672eafb864607c2667033b
rperebalv-gif <rperebalv@miumg.edu.gt>
2026-04-18 17:50:50 -0600

    Add files via upload
    LAB 2

---
+++ login1/src/main/java/Mavenproject2.java
@@ -0,0 +1,20 @@
+
+import com.mycompany.login1.menu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author umg
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+           menu me = new menu();
+           me.setVisible(true);
+    }
+}
---
+++ login1/src/main/java/com/mycompany/login1/Login1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login1;
+
+/**
+ *
+ * @author umg
+ */
+public class Login1 {
+
+    public static void main(String[] args) {
+    JFrame jf= new JFrame ();
+    jf.setVisible(true);
+    
+    
+    
+    
+    
+    }
+}
+
