bd139ce545ae32645ec16b5f3d25fc4e5d624488
rperebalv-gif <rperebalv@miumg.edu.gt>
2026-04-24 13:47:53 -0600

    Add files via upload

---
+++ login1/Login_Sign/src/main/java/myfirstproyect/com/login_sign/LoginInfoDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myfirstproyect.com.login_sign;
+
+/**
+ *
+ * @author d4nil
+ */
+class LoginInfoDAO {
+
+    void changePassword(String op, String np) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ login1/Login_Sign/src/main/java/myfirstproyect/com/login_sign/Login_Sign.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package myfirstproyect.com.login_sign;
+
+/**
+ *
+ * @author d4nil
+ */
+public class Login_Sign {
+
+    public static void main(String[] args) {
+        login1 lg = new login1();
+        lg.setVisible(true);
+    }
+}
